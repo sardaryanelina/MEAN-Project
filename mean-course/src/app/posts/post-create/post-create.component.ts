ccef9fee150efd7c6062a94554171082e4b0116e
sardaryanelina <87990387+sardaryanelina@users.noreply.github.com>
2022-08-19 13:08:27 -0400

    Adding our first component, Listening to Events and Outputting Content

---
+++ mean-course/src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html',
+})
+export class PostCreateComponent {
+  newPost = 'NO CONTENT';
+  onAddPost() {
+    // alert('Post added!');
+    this.newPost = "The user's post";
+  }
+}
