5002c4dbf5c4a55ec7d158422503d5cd89a2750a
sardaryanelina <87990387+sardaryanelina@users.noreply.github.com>
2022-08-25 13:47:09 -0400

    Adding a Toolbar - Adding a Header

--- my-project/src/app/app.module.ts
+++ my-project/src/app/app.module.ts
@@ -5,19 +5,23 @@ import { FormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 import { AppComponent } from './app.component';
 import { BookCreateComponent } from './books/book-create/book-create.component';
+import { HeaderComponent } from './header/header.component';
+
 
 @NgModule({
-  declarations: [AppComponent, BookCreateComponent],
+  declarations: [AppComponent, BookCreateComponent, HeaderComponent],
   imports: [
     BrowserModule,
     FormsModule,
     BrowserAnimationsModule,
     MatInputModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent],
