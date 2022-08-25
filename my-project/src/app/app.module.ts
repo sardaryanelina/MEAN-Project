f357cf0c058e9aa8f774d47bd5f7c48cec361fe5
sardaryanelina <87990387+sardaryanelina@users.noreply.github.com>
2022-08-25 01:38:09 -0400

    Installing Angular Material - ng add @angular/material

--- my-project/src/app/app.module.ts
+++ my-project/src/app/app.module.ts
@@ -1,13 +1,24 @@
-import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
 
 import { AppComponent } from './app.component';
 import { BookCreateComponent } from './books/book-create/book-create.component';
 
 @NgModule({
   declarations: [AppComponent, BookCreateComponent],
-  imports: [BrowserModule, FormsModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- my-project/src/app/books/book-create/book-create.component.ts
+++ my-project/src/app/books/book-create/book-create.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-book-create',
   templateUrl: './book-create.component.html',
+  styleUrls: ['./book-create.component.css'],
 })
 export class BookCreateComponent {
   enteredValue = '';
