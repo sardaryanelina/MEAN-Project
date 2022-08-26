3ca893d5ad81b16ac988a28305dd7d3fb928c671
sardaryanelina <87990387+sardaryanelina@users.noreply.github.com>
2022-08-25 23:41:17 -0400

    Outputting hard-coded data

--- my-project/src/app/books/book-list/book-list.component.ts
+++ my-project/src/app/books/book-list/book-list.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./book-list.component.css']
 })
 export class BookListComponent {
-  posts = [
+  books = [
     {title:'First Book title', author: 'author name 1', publisher: ' publisher name 1', price: 11 },
     {title:'Second Book title', author: 'author name 2', publisher: ' publisher name 2', price: 12 },
     {title:'Third Book title', author: 'author name 3', publisher: ' publisher name 3', price: 13 }
