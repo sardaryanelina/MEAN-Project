4eafbcf3ed3fd7744c1fa2b363ffa6b5162753ca
sardaryanelina <87990387+sardaryanelina@users.noreply.github.com>
2022-08-28 02:29:55 -0400

    Fetching Data From Database

--- my-project/backend/app.js
+++ my-project/backend/app.js
@@ -46,34 +46,13 @@ app.post("/api/books", (req, res, next) => {
 });
 
 app.get('/api/books', (req, res, next) => {
-  const books = [
-    {
-      id: '111testId12345678',
-      title: 'First title',
-      author: 'First author',
-      description: 'Frist description',
-      publisher: 'First publisher',
-      publishedDate: new Date(2011, 01, 01),
-      pageCount: 111,
-      language: 'English',
-      price: 11
-    },
-    {
-      id: '222testId12345678',
-      title: 'Second title',
-      author: 'Second author',
-      description: 'Second description',
-      publisher: 'Second publisher',
-      publishedDate: new Date('February 2, 2022'),
-      pageCount: 222,
-      language: 'English',
-      price: 22
-    }
-  ];
-  res.status(200).json({
-    message: 'Books fetched successfully',
-    books: books
-  });
+  Book.find()
+    .then(documents => {
+      res.status(200).json({
+        message: 'Books fetched successfully',
+        books: documents
+      });
+    });
 });
 
 module.exports = app;
