4b81732356e71b3e92f63ce0de229721a5919321
sardaryanelina <87990387+sardaryanelina@users.noreply.github.com>
2022-08-28 00:18:02 -0400

    CORS

--- my-project/backend/app.js
+++ my-project/backend/app.js
@@ -2,6 +2,16 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS"
+  );
+  next();
+});
+
 app.use('/api/books', (req, res, next) => {
   const books = [
     {
