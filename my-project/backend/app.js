27b47002f58d1da74c9b91fbf16224031664180b
sardaryanelina <87990387+sardaryanelina@users.noreply.github.com>
2022-08-27 23:47:47 -0400

    Fetching Initial Books

--- my-project/backend/app.js
+++ my-project/backend/app.js
@@ -2,13 +2,35 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('Middlware');
-  next();
-});
-
-app.use((req, res, next) => {
-  res.send('Hello from express');
+app.use('/api/books', (req, res, next) => {
+  const posts = [
+    {
+      id: '111testId12345678',
+      title: 'First title',
+      author: 'First author',
+      description: 'Frist description',
+      publisher: 'First publisher',
+      publishedDate: new Date(2011, 01, 01),
+      pageCount: 111,
+      language: 'English',
+      price: 11
+    },
+    {
+      id: '222testId12345678',
+      title: 'Second title',
+      author: 'Second author',
+      description: 'Second description',
+      publisher: 'Second publisher',
+      publishedDate: new Date('February 2, 2022'),
+      pageCount: 222,
+      language: 'English',
+      price: 22
+    }
+  ];
+  res.status(200).json({
+    message: 'Posts fetched successfully',
+    posts: posts
+  });
 });
 
 module.exports = app;
