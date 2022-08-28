2bb4f2c519ca6ab3d31c8954bc85b8e73fff35e2
sardaryanelina <87990387+sardaryanelina@users.noreply.github.com>
2022-08-28 02:05:57 -0400

    Connecting our Node Express App to MongoDB

--- my-project/backend/app.js
+++ my-project/backend/app.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const Book = require('./models/book');
 
 const app = express();
+
+mongoose.connect("mongodb+srv://TeamProject:database@bookstore.2kfo5.mongodb.net/?retryWrites=true&w=majority")
+  .then(() => {
+    console.log('Connected to database!');
+  })
+  .catch(() => {
+    console.log('Connection failed.');
+  });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));//we do not need for this app
